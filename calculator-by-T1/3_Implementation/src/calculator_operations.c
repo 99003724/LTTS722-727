d08d1b349332847e4312ccf69de41c9b0cd2eedd
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-14 20:57:55 +0530

    src file operations added

--- calculator-by-T1/3_Implementation/src/calculator_operations.c
+++ calculator-by-T1/3_Implementation/src/calculator_operations.c
@@ -26,3 +26,32 @@ float modulous(int operand1 , int operand2)
 {
     return operand1 % operand2;
 }
+double square(double sq)
+{
+    return sq*sq;
+}
+
+double cube(double cu)
+{
+    return cu*cu*cu;
+}
+
+double sqrt(double sq_rt)
+{
+    return sqrt(sq_rt);
+}
+
+double cbrt(double cb_rt)
+{
+    return cbrt(cb_rt);
+}
+ int factorial(int number)
+{
+  int iteration, factorial=1;
+  for(iteration=1; iteration<=number; iteration++)
+  {
+    	factorial=factorial*iteration;
+        }
+  return factorial;
+  }
+
