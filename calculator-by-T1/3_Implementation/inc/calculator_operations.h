b3b1c5e9d78070e1ba260ebe147dc741e22fa6c6
patamsetti sai kalki <78849373+99003725@users.noreply.github.com>
2021-02-14 10:10:31 -0800

    modified int to float

--- calculator-by-T1/3_Implementation/inc/calculator_operations.h
+++ calculator-by-T1/3_Implementation/inc/calculator_operations.h
@@ -16,7 +16,7 @@
 * @param[in] operand2 
 * @return Result of operand1 + operand2
 */
-int add(int operand1, int operand2);
+int addition(int operand1, int operand2);
 
 /**
 *  subtracts the operand1, operand2 and returns the result
@@ -24,7 +24,7 @@ int add(int operand1, int operand2);
 * @param[in] operand2 
 * @return Result of operand1 - operand2
 */
-int substract(int operand1, int operand2);
+int substraction(int operand1, int operand2);
 
 /**
 *  multiply the operand1, operand2 and returns the result
@@ -32,7 +32,7 @@ int substract(int operand1, int operand2);
 * @param[in] operand2 
 * @return Result of operand1 * operand2
 */
-int multiply(int operand1, int operand2);
+double multiplication(int operand1, int operand2);
 
 
 /**
@@ -42,7 +42,14 @@ int multiply(int operand1, int operand2);
 * @return integer value of the operand1 / operand2
 * @note returns 0 for divide by 0 error
 */
-double divide(int operand1, int operand2);
+double division(int operand1, int operand2);
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] operand1 
+* @param[in] operand2 
+* @return integer value of the operand1 % operand2
+* @note returns result of operand1 % operand2
+*/
 float modulous(int operand1, int operand2);
 /**
 *  finds square of the number
