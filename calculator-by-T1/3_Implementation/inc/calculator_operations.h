8e5246fcb7ccb198dc7c0b67ba14b62a3e554971
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-14 20:42:53 +0530

    Mathematical operations added

--- calculator-by-T1/3_Implementation/inc/calculator_operations.h
+++ calculator-by-T1/3_Implementation/inc/calculator_operations.h
@@ -44,5 +44,42 @@ int multiply(int operand1, int operand2);
 */
 double divide(int operand1, int operand2);
 float modulous(int operand1, int operand2);
+/**
+*  finds square of the number
+* @param[in]  sq 
+* @return Result of sq*sq
+*/
+double square(double sq);
+
+/**
+*  finds cube of the number
+* @param[in] cu 
+* @return Result of cu*cu*cu
+*/
+double cube(double cu);
+
+/**
+*  finds square root of the number
+* @param[in] sq_rt
+* @return Result of sqrt(sq_rt)
+*/
+double square_root(double sq_rt);
+
+
+/**
+* finds cube root of the number 
+* @param[in] cu_rt  
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+double cube_root(double cu_rt);
+
+
+/**
+* finds factorial of the number 
+* @param[in]  
+* @return factorial
+*/
+int factorial(int number);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
