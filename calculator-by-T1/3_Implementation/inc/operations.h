cc6aa33da62c11dda4b2166748206474f35b4433
99003724 <78853934+99003724@users.noreply.github.com>
2021-02-14 16:28:29 +0530

    modified code

--- calculator-by-T1/3_Implementation/inc/operations.h
+++ calculator-by-T1/3_Implementation/inc/operations.h
@@ -1,32 +1,46 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef RESULT_H
+#define RESULT_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-     
-double square(double sq)
-{
-return(sq*sq);
-}
-double cube(double cu)
-{
-    return (cu*cu*cu);
-}
-double square_root(double sq_rt)
-{
-    return sqrt(sq_rt);
-}
+#include "result.h"
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in]  sq 
+* @return Result of sq*sq
+*/
+double square(double sq);
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] cu 
+* @return Result of cu*cu*cu
+*/
+double cube(double cu);
+
+/**
+*  multiply the operand1, operand2 and returns the result
+* @param[in] sq_rt
+* @return Result of sqrt(sq_rt)
+*/
+double square_root(double sq_rt);
+
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in] cu_rt  
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
 double cube_root(double cu_rt);
-{
-    return cbrt(cu_rt);
-}
-int factorial(int number)
-{
-  int iteration, factorial=1;
-  for(iteration=1; iteration<=number; iteration++)
-  {
-    	factorial=factorial*iteration;
-  }
-  return factorial;
-}
-#endif
+
+
+/**
+* divides the operand1 by operand2 and returns the result 
+* @param[in]  
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+int factorial(int number);
+#endif  /* #define RESULT_H */
