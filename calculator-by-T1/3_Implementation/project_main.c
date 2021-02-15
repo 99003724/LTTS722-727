ec843ce826f87f0d59859c4ef81f28f5dc8ecc13
99003727 <shiva.vankadhara@ltts.com>
2021-02-15 11:59:16 +0530

    merge by shiva

--- calculator-by-T1/3_Implementation/project_main.c
+++ calculator-by-T1/3_Implementation/project_main.c
@@ -11,24 +11,27 @@
         float degree,rad;
         printf("what do you want convert?give the choice as 1Degree. or 2.Radian\n");
         scanf("%d",&choice3);
-        switch(choice3){
+        switch(choice3)
+        {
         /**char b;
         scanf("%c",&b);
         if(b=='D')**/
-        case 1:
-        {
-           printf("give the data to convert from degrees to radian\n");
-           scanf("%f",&degree);
-           printf("%f", Radian(degree));
-           break;
+            case 1:
+            {
+                printf("give the data to convert from degrees to radian\n");
+                scanf("%f",&degree);
+                printf("%f", Radian(degree));
+                break;
+            }
+            case 2:
+            {
+                printf("give the data to convert from radian to degrees\n");
+                scanf("%f",&rad);
+                printf("%f",Degree(rad));
+                break;//asdasd
+            }
+            default : printf('it should not come here');
         }
-        case 2:
-        {
-           printf("give the data to convert from radian to degrees\n");
-           scanf("%f",&rad);
-           printf("%f",Degree(rad));
-           break;
-        }}
     }
 
 }
